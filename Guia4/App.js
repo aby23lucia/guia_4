9bb4f238602329705832a4fdb5af6ee8a0838ba4
aby23lucia <48072445+aby23lucia@users.noreply.github.com>
2022-09-09 14:16:23 -0600

    Add files via upload

---
+++ Guia4/App.js
@@ -0,0 +1,121 @@
+/**
+* @format
+* @flow strict-local
+*/
+import React, {useState,useEffect} from 'react';
+
+import{ SafeAreaView, StyleSheet, View, Text,StatusBar}from 'react-native';
+import colors from './src/utils/colors';
+import Form from './src/components/Form';
+
+import Footer from './src/components/Footer';
+
+import Result from './src/components/Result';
+
+export default function App() {
+  const [capital,setCapital]=useState(null);
+  const [interest,setInterest]=useState(null);
+  const [months,setMonths]=useState(null);
+
+const [total, setTotal] = useState(null);
+ const [errorMessage, setErrorMessage] = useState('');
+useEffect(() => {
+ if (capital && interest && months) calculate();
+ else reset();
+ }, [capital, interest, months]);
+ const calculate = () => {
+ reset();
+
+
+  if (!capital) {
+ setErrorMessage('Añade la cantidad que quieres solicitar');
+ } else if (!interest) {
+ setErrorMessage('Añade el interes del prestamos');
+ } else if (!months) {
+ setErrorMessage('Seleccióna los meses a pagar');
+ } else {
+ const i = interest / 100;
+ const fee = capital / ((1 - Math.pow(i + 1, -months)) / i);
+ setTotal({
+ monthlyFee: fee.toFixed(2).replace('.', ','),
+ totalPayable: (fee * months).toFixed(2).replace('.', ','),
+ });
+ }
+ };
+ const reset = () => {
+ setErrorMessage('');
+ setTotal(null);
+ };
+
+  return (
+    <>
+    <StatusBar barStyle="light-content"/>
+    <SafeAreaView style={styles.Header}>
+    <Text style={styles.HeadApp}>COTIZADOR DE PRESTAMOS</Text>
+    <Form
+    setCapital={setCapital}
+    setInterest={setInterest}
+    setMonths={setMonths}
+    />
+    
+    
+    </SafeAreaView>
+
+    <Result
+ capital={capital}
+ interest={interest}
+ months={months}
+ total={total}
+ errorMessage={errorMessage}
+ />
+ <Footer calculate={calculate} />
+
+    
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  Header:{
+    backgroundColor:colors.PRIMARY_COLOR,
+    height:200,
+    borderBottomLeftRadius:30,
+    borderBottomRightRadius:30,
+    alignItems:'center'
+ 
+  },
+  HeadApp:{
+    fontSize:21,
+    fontWeight:'bold',
+    color:'#ffg',
+    marginTop:15,
+  },
+
+});
+
+const style=StyleSheet.create({
+  safeArea:{
+    height:'center',
+  },
+  backgroundColor:{
+    backgroundColor:colors.PRIMARY_COLOR,
+    height:100,
+    width:'100%',
+    borderBottomLeftRadius:30,
+    borderBottomRightRadius:30,
+    position:'absolute',
+    zIndex:-1,
+  },
+  titleApp:{
+    fontSize:25,
+    fontWeight:'bold',
+    color:'#fff',
+    marginTop:15,
+  },
+  
+})
+
+
+
+
+
